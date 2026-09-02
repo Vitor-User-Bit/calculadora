abb43454ff1461703e5b57e244f7aa46def650ff
Vitor-User-Bit <cezervitor@gmail.com>
2026-09-01 21:24:39 -0300

    first commit

---
+++ GO/maps.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+
+
+	anoNasc := map[string]int{
+		"steph": 1995,
+		"bento": 2008,
+	}
+	fmt.Println(anoNasc)
+	fmt.Println(anoNasc["steph"])
+	fmt.Println(anoNasc["bento"])
+
+
+
+}
---
+++ go.mod
@@ -0,0 +1,4 @@
+module vitordev
+
+go 1.26.5
+ 
