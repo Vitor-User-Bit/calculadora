de99f521efd77c72d2e1e0ed2ee55f3fd1cc778e
Vitor-User-Bit <cezervitor@gmail.com>
2026-09-03 10:36:20 -0300

    percorrendolistacomfor

---
+++ array.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+
+var listaCompras [5]string 
+
+listaCompras[0] = "Arroz\n"
+listaCompras[1] = "Feijão\n"
+listaCompras[2] = "Macarrão\n"
+listaCompras[3] = "Leite\n"
+listaCompras[4] = "Café\n"
+
+//fmt.Println("Lista de Compras:", listaCompras)
+//fmt.Println("Só o segundo item:", listaCompras[1])
+
+for i := 0; i < len(listaCompras); i++ {
+	fmt.Println("Item", i, ":", listaCompras[i])
+}
+
+}
